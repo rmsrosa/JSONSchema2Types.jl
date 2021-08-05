2d7b1804ee5bca73213cf4dff653ad70414dadd8
Ricardo Rosa <rmsrosa@gmail.com>
2021-08-05 12:25:56 -0300

    add  HTTP and read beerjson from web

---
+++ playground/parse_beerjson.jl
@@ -0,0 +1,15 @@
+using JSON3
+using HTTP
+
+include("../src/parser.jl")
+
+uri = "https://raw.githubusercontent.com/beerjson/beerjson/master/json/beer.json"
+@assert isvalid(HTTP.URI(uri)) "Invalid URI"
+json_string = String(HTTP.get(uri).body)
+json_schema = JSON3.read(json_string)
+
+folder_for_generated_types = "../examples/beerjson_parsed_schemas/"
+
+generate_types(json_schema;
+    gen_folder=folder_for_generated_types,
+    module_name = "BeerJSON")
--- playground/read_schema.jl
+++ playground/read_schema.jl
@@ -1,8 +1,19 @@
 using JSON3
+using HTMl
 
 json_string = read("jsonschema_examples/beerjson-1.0.1/json/beer.json", String)
 json_schema = JSON3.read(json_string)
 
+@show keys(json_schema)
+for key in (Symbol("\$schema"), Symbol("\$id"))
+    if key in keys(json_schema)
+        println("$key: $(json_schema[key])")
+    end
+end
+
+url = "https://raw.githubusercontent.com/beerjson/beerjson/master/json/beer.json"
+json_string = String(HTTP.get(url).body)
+json_schema = JSON3.read(json_string)
 @show keys(json_schema)
 for key in (Symbol("\$schema"), Symbol("\$id"))
     if key in keys(json_schema)
