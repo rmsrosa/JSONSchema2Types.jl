629650a5f732f30059f933e3cbd0d9ffe401fd0c
Ricardo Rosa <rmsrosa@gmail.com>
2025-09-24 16:17:33 -0300

    add example schema with oneOf, allOf, anyOf, and not keywords

--- playground/parse_gemini.jl
+++ playground/parse_gemini.jl
@@ -2,4 +2,6 @@ include("../src/json_schema_to_julia_gemini.jl")
 
 generate_julia_types("examples/gemini_schemas/order_schema.json", "examples/gemini_parsed_schemas/schema_types.jl")
 
+generate_julia_types("examples/gemini_schemas/combined_schema.json", "examples/gemini_parsed_schemas/combined_schema_types.jl")
+
 generate_julia_types("examples/beerjson-1.0.1/json/beer.json", "examples/beerjson_parsed_schemas/beerjson_schema_types_gemini.jl")
